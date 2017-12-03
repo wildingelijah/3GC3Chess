ea1ad38a642f205b49ba1f358ff009509b362ae1
leemmoo <aleemulhaq@gmail.com>
2017-12-03 14:43:34 -0500

    Attempt to fix seg fault for pieces

--- project/project.cpp
+++ project/project.cpp
@@ -41,14 +41,12 @@ int pawnObj;
 
 
 //********************************************************************
-//for vertexes and vectors
-struct coordinate{
+struct coordinate{ //for vertices
 	float x,y,z;
 	coordinate(float a,float b,float c) : x(a),y(b),z(c) {};
 };
 
-//for faces, it can contain triangles and quads as well, the four variable contain which is that
-struct face{
+struct face{ //face can be made from quad or triangle
 	int facenum;
 	bool four;
 	int faces[4];
@@ -58,7 +56,7 @@ struct face{
 		faces[2]=f3;
 		four=false;
 	}
-	face(int facen,int f1,int f2,int f3,int f4) : facenum(facen){ //overloaded constructor for quads
+	face(int facen,int f1,int f2,int f3,int f4) : facenum(facen){ //constructor for quads
 		faces[0]=f1;
 		faces[1]=f2;
 		faces[2]=f3;
@@ -67,14 +65,11 @@ struct face{
 	}
 };
 
-float angle=0.0;	//we rotate or object with angle degrees
-
 int loadObject(const char* filename)
 {
-	std::vector<std::string*> coord;	//read every single line of the obj file as a string
+	std::vector<std::string*> coord;//every single line of the obj file as a string
 	std::vector<coordinate*> vertex;
 	std::vector<face*> faces;
-	std::vector<coordinate*> normals;	//normal vectors for every face
 	std::ifstream in(filename);	//open the .obj file
 	if(!in.is_open())	//if not opened, exit with -1
 	{
@@ -88,55 +83,43 @@ int loadObject(const char* filename)
 		in.getline(buf,256);
 		coord.push_back(new std::string(buf));
 	}
-	//go through all of the elements of coord, and decide what kind of element is that
+	//read all strings and their first character to see if Vertex, face or comment etc.
 	for(int i=0;i<coord.size();i++)
 	{
-		if((*coord[i])[0]=='#')	//if it is a comment (the first character is #)
-			continue;	//we don't care about that
-		else if((*coord[i])[0]=='v' && (*coord[i])[1]==' ')	//if vector
+		if((*coord[i])[0]=='v' && (*coord[i])[1]==' ')	//if vector & not vn
 		{
 			float tmpx,tmpy,tmpz;
-			sscanf(coord[i]->c_str(),"v %f %f %f",&tmpx,&tmpy,&tmpz);	//read in the 3 float coordinate to tmpx,tmpy,tmpz
-			vertex.push_back(new coordinate(tmpx,tmpy,tmpz));	//and then add it to the end of our vertex list
-		}else if((*coord[i])[0]=='v' && (*coord[i])[1]=='n')	//if normal vector
-		{
-			float tmpx,tmpy,tmpz;	//do the same thing
-			sscanf(coord[i]->c_str(),"vn %f %f %f",&tmpx,&tmpy,&tmpz);
-			normals.push_back(new coordinate(tmpx,tmpy,tmpz));	
+			sscanf(coord[i]->c_str(),"v %f %f %f",&tmpx,&tmpy,&tmpz);	//store to 3 variables tmpx,tmpy,tmpz
+			vertex.push_back(new coordinate(tmpx,tmpy,tmpz));	// push to vertex vector
 		}else if((*coord[i])[0]=='f')	//if face
 		{
 			int a,b,c,d,e;
-			if(count(coord[i]->begin(),coord[i]->end(),' ')==3)	//if it is a triangle (it has 3 space in it)
+			if(count(coord[i]->begin(),coord[i]->end(),' ')==3)	//if it is a triangle
 			{
 		  sscanf(coord[i]->c_str(),"f %d//%d %d//%d %d//%d",&a,&b,&c,&b,&d,&b);
-				faces.push_back(new face(b,a,c,d));	//read in, and add to the end of the face list
+				faces.push_back(new face(b,a,c,d));	//push to face vector
 			}else{
 				sscanf(coord[i]->c_str(),"f %d//%d %d//%d %d//%d %d//%d",&a,&b,&c,&b,&d,&b,&e,&b); //quads
-				faces.push_back(new face(b,a,c,d,e));	//do the same, except we call another constructor, and we use different pattern
+				faces.push_back(new face(b,a,c,d,e));	//push to face vector
 			}
 		}
 	}
 	//draw
-	int num;	//the id for the list
-	num=glGenLists(1);	//generate a uniqe
-	glNewList(num,GL_COMPILE);	//and create it
+	int num;//the id for the list
+	num=glGenLists(1);
+	glNewList(num,GL_COMPILE);
 	for(int i=0;i<faces.size();i++)	
 	{
-        if (faces[i]->four) //if it's a quad draw a quad
+        if (faces[i]->four) //quad
         {
 			glBegin(GL_QUADS);
-				//basically all I do here, is use the facenum (so the number of the face) as an index for the normal, so the 1st normal owe to the first face
-				//I subtract 1 because the index start from 0 in C++
-				glNormal3f(normals[faces[i]->facenum-1]->x,normals[faces[i]->facenum-1]->y,normals[faces[i]->facenum-1]->z);
-				//draw the faces
 				glVertex3f(vertex[faces[i]->faces[0]-1]->x,vertex[faces[i]->faces[0]-1]->y,vertex[faces[i]->faces[0]-1]->z);
 				glVertex3f(vertex[faces[i]->faces[1]-1]->x,vertex[faces[i]->faces[1]-1]->y,vertex[faces[i]->faces[1]-1]->z);
 				glVertex3f(vertex[faces[i]->faces[2]-1]->x,vertex[faces[i]->faces[2]-1]->y,vertex[faces[i]->faces[2]-1]->z);
 				glVertex3f(vertex[faces[i]->faces[3]-1]->x,vertex[faces[i]->faces[3]-1]->y,vertex[faces[i]->faces[3]-1]->z);
 			glEnd();
 		}else{
-			glBegin(GL_TRIANGLES);
-				glNormal3f(normals[faces[i]->facenum-1]->x,normals[faces[i]->facenum-1]->y,normals[faces[i]->facenum-1]->z);
+			glBegin(GL_TRIANGLES); //triangle
 				glVertex3f(vertex[faces[i]->faces[0]-1]->x,vertex[faces[i]->faces[0]-1]->y,vertex[faces[i]->faces[0]-1]->z);
 				glVertex3f(vertex[faces[i]->faces[1]-1]->x,vertex[faces[i]->faces[1]-1]->y,vertex[faces[i]->faces[1]-1]->z);
 				glVertex3f(vertex[faces[i]->faces[2]-1]->x,vertex[faces[i]->faces[2]-1]->y,vertex[faces[i]->faces[2]-1]->z);
@@ -149,8 +132,6 @@ int loadObject(const char* filename)
 		delete coord[i];
 	for(int i=0;i<faces.size();i++)
 		delete faces[i];
-	for(int i=0;i<normals.size();i++)
-		delete normals[i];
 	for(int i=0;i<vertex.size();i++)
 		delete vertex[i];
 	return num;	//return with the id
